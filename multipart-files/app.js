bb366977d6e18c7b55416e5ed38dfc693d057ed7
Jonathan Ong <jonathanrichardong@gmail.com>
2013-11-28 15:34:56 -0800

    bump archan

--- multipart-files/app.js
+++ multipart-files/app.js
@@ -9,18 +9,21 @@ var archan = require('archan');
 var app = module.exports = koa();
 
 app.use(function *(){
+  // create a go-like channel for control flow
   var ch = archan();
+
+  // parse the multipart body
   var parts = parse(this, {
     autoFields: true
   });
 
   // create a temporary folder to store files
   var tmpdir = path.join(os.tmpdir(), Math.random().toString(36).slice(2));
-  // make the directory
-  yield function (done) {
-    fs.mkdir(tmpdir, done);
-  }
 
+  // make the directory
+  yield fs.mkdir.bind(null, tmpdir);
+
+  // yield each part as a stream
   var part;
   while (part = yield parts) {
     // save each part to a file,
@@ -29,15 +32,8 @@ app.use(function *(){
     saveTo(part, path.join(tmpdir, part.filename), ch.push());
   }
 
-  // create an array of files
-  var files = [];
-  var file;
-  while (file = yield* ch.shift()) {
-    files.push(file)
-  }
-
-  // return the array of filenames
-  this.body = files;
+  // return all the filenames as an array
+  this.body = yield* ch.end();
 })
 
 if (!module.parent) app.listen(3000);
