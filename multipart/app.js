b1e3d56118cc6c2092bb12981ce3334b8480ff8a
Jonathan Ong <jonathanrichardong@gmail.com>
2013-11-29 16:15:03 -0800

    multipart: bump archan, add concurrency control

--- multipart/app.js
+++ multipart/app.js
@@ -10,22 +10,26 @@ var app = module.exports = koa();
 
 app.use(function *(){
   // create a go-like channel for control flow
-  var ch = archan();
+  var ch = archan({
+    concurrency: 1 // save only 1 file at a time
+  });
 
   // parse the multipart body
   var parts = parse(this, {
-    autoFields: true
+    autoFields: true // saves the fields to parts.field(s)
   });
 
   // create a temporary folder to store files
   var tmpdir = path.join(os.tmpdir(), Math.random().toString(36).slice(2));
 
-  // make the directory
+  // make the temporary directory
   yield fs.mkdir.bind(null, tmpdir);
 
   // yield each part as a stream
   var part;
   while (part = yield parts) {
+    // wait if there are too many file descriptors opened
+    yield* ch.drain();
     // save each part to a file,
     // but do it in a different channel
     // so we don't block this particular while loop.
@@ -34,7 +38,7 @@ app.use(function *(){
 
   // return all the filenames as an array
   // after all the files have finished downloading
-  this.body = yield* ch.end();
+  this.body = yield* ch.flush();
 })
 
 if (!module.parent) app.listen(3000);
