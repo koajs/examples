4a1ee93c1cdc279262ea201889c7146267ff159a
Jonathan Ong <jonathanrichardong@gmail.com>
2013-12-06 14:09:41 -0800

    add the koa-compose example

---
+++ compose/app.js
@@ -0,0 +1,58 @@
+/**
+ * Each `app.use()` only accepts a single generator function.
+ * If you want to combine multiple generator functions into a single one,
+ * you can use `koa-compose` to do so.
+ * This allows you to use `app.use()` only once.
+ * Your code will end up looking something like:
+ *
+ *   app.use(compose([
+ *     function *(){},
+ *     function *(){},
+ *     function *(){}
+ *   ]))
+*/
+
+
+var compose = require('koa-compose');
+var koa = require('koa');
+var app = module.exports = koa();
+
+// x-response-time
+
+function *responseTime(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  this.set('X-Response-Time', ms + 'ms');
+}
+
+// logger
+
+function* logger(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  if ('test' != process.env.NODE_ENV) {
+    console.log('%s %s - %s', this.method, this.url, ms);
+  }
+}
+
+// response
+
+function* respond(next){
+  yield next;
+  if ('/' != this.url) return;
+  this.body = 'Hello World';
+}
+
+// composed middleware
+
+var all = compose([
+  responseTime,
+  logger,
+  respond
+]);
+
+app.use(all);
+
+if (!module.parent) app.listen(3000);
---
+++ compose/test.js
@@ -0,0 +1,28 @@
+var app = require('./app');
+var request = require('supertest').agent(app.listen());
+
+describe('Compose', function(){
+  describe('when GET /', function(){
+    it('should say "Hello World"', function(done){
+      request
+      .get('/')
+      .expect(200)
+      .expect('Hello World', done);
+    })
+
+    it('should set X-Response-Time', function(done){
+      request
+      .get('/')
+      .expect('X-Response-Time', /ms$/)
+      .expect(200, done);
+    })
+  })
+
+  describe('when not GET /', function(){
+    it('should 404', function(done){
+      request
+      .get('/aklsjdf')
+      .expect(404, done);
+    })
+  })
+})
