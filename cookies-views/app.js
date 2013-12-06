d951e3e2249d2b9bb77e12d6206563c27ae4fdd8
Jonathan Ong <jonathanrichardong@gmail.com>
2013-12-06 14:17:50 -0800

    add the cookies-views example

---
+++ cookies-views/app.js
@@ -0,0 +1,15 @@
+/**
+ * This example simply sets the number of views from the same client
+ * both as a cookie and as a response string.
+ */
+
+var koa = require('koa');
+var app = module.exports = koa();
+
+app.use(function *(){
+  var n = ~~this.cookies.get('view') + 1;
+  this.cookies.set('view', n);
+  this.body = n + ' views';
+});
+
+if (!module.parent) app.listen(3000);
---
+++ cookies-views/test.js
@@ -0,0 +1,16 @@
+var app = require('./app');
+var request = require('supertest').agent(app.listen());
+
+describe('Cookies Views', function(){
+  [1, 2, 3].forEach(function (i) {
+    describe('on iteration #' + i, function(){
+      it('should set the views as a cookie and as the body', function(done){
+        request
+        .get('/')
+        .expect(200)
+        .expect('Set-Cookie', new RegExp('view=' + i))
+        .expect(i + ' views', done);
+      })
+    })
+  })
+})
