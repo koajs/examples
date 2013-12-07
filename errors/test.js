2f69674c6b17df04e6c1f226afed077de023cbf5
Jonathan Ong <jonathanrichardong@gmail.com>
2013-12-07 14:52:51 -0800

    add errors example

---
+++ errors/test.js
@@ -0,0 +1,23 @@
+var app = require('./app');
+var request = require('supertest').agent(app.listen());
+
+describe('Errors', function () {
+  it('should catch the error', function(done){
+    request
+    .get('/')
+    .expect(500)
+    .expect('Content-Type', /text\/html/, done);
+  })
+
+  it('should emit the error on app', function(done){
+    app.once('error', function (err, ctx) {
+      err.message.should.equal('boom boom');
+      ctx.should.be.ok;
+      done();
+    })
+
+    request
+    .get('/')
+    .end(function(){});
+  })
+})
