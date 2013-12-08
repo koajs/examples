d25778272585806877c6a5aa10bf4df93421bd32
TJ Holowaychuk <tj@vision-media.ca>
2013-12-08 10:57:51 -0800

    add tests for `templates`

---
+++ templates/index.js
@@ -0,0 +1,28 @@
+
+var views = require('co-views');
+var koa = require('koa');
+var app = module.exports = koa();
+
+// setup views, appending .ejs
+// when no extname is given to render()
+
+var render = views(__dirname + '/views', { ext: 'ejs' });
+
+// dummy data
+
+var user = {
+  name: {
+    first: 'Tobi',
+    last: 'Holowaychuk'
+  },
+  species: 'ferret',
+  age: 3
+};
+
+// render
+
+app.use(function *(){
+  this.body = yield render('user', { user: user });
+})
+
+if (!module.parent) app.listen(4000);
---
+++ templates/test.js
@@ -0,0 +1,13 @@
+var app = require('./');
+var request = require('supertest').agent(app.listen());
+
+describe('Templates', function(){
+  describe('GET /', function(){
+    it('should respond with a rendered view', function(done){
+      request
+      .get('/')
+      .expect(200)
+      .expect('<p>Tobi is a 3 year old ferret.</p>', done);
+    })
+  })
+})
