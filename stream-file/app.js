f259703a9c2486fa4e336fe4621b0984fd68de3e
TJ Holowaychuk <tj@vision-media.ca>
2013-12-08 10:36:28 -0800

    refactor stream-file example to add content-type

--- stream-file/app.js
+++ stream-file/app.js
@@ -2,18 +2,15 @@
 var koa = require('koa');
 var fs = require('fs');
 var app = module.exports = koa();
+var path = require('path');
+var extname = path.extname;
 
 // try GET /app.js
 
 app.use(function *(){
   var path = __dirname + this.path;
-  if (yield exists(path)) this.body = fs.createReadStream(path).on('error', this.onerror);
+  this.type = extname(path);
+  this.body = fs.createReadStream(path);
 });
-
-function exists(file) {
-  return function(done){
-    fs.stat(file, done);
-  }
-}
 
 if (!module.parent) app.listen(3000);
