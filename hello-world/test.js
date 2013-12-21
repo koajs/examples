38fbeeaa7fb19b10f9659a4d48f4d6248c25f614
yosssi <yoshida.keiji.84@gmail.com>
2013-12-21 22:38:12 +0900

    Fixed some typo errors.

--- hello-world/test.js
+++ hello-world/test.js
@@ -1,11 +1,11 @@
-var app = require('./app')
+var app = require('./app');
 var request = require('supertest').agent(app.listen());
 
-describe('Hello Word', function(){
-  it('should say "Hello Word"', function(done){
+describe('Hello World', function(){
+  it('should say "Hello World"', function(done){
     request
     .get('/')
     .expect(200)
     .expect('Hello World', done);
-  })
-})
+  });
+});
