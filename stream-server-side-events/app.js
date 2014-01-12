f624546735ddf471bcf2887f0b105a6e3b118182
Jonathan Ong <jonathanrichardong@gmail.com>
2014-01-11 16:22:10 -0800

    add SSE example. needs tests

---
+++ stream-server-side-events/app.js
@@ -0,0 +1,29 @@
+
+var koa = require('koa');
+var app = module.exports = koa();
+
+var sse = require('./sse');
+var db = require('./db');
+
+app.use(function* () {
+  // otherwise node will automatically close this connection in 2 minutes
+  this.req.setTimeout(Infinity);
+
+  this.type = 'text/event-stream; charset=utf-8';
+
+  var body = this.body = sse();
+  var stream = db.subscribe('some event');
+  stream.pipe(body);
+
+  // if the connection closes or errors,
+  // we stop the SSE.
+  var socket = this.socket;
+  socket.on('error', close);
+  socket.on('close', close);
+
+  function close() {
+    stream.unpipe(body);
+    socket.removeListener('error', close);
+    socket.removeListener('close', close);
+  }
+})
