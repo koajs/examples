6d0c74a06eb5a7fa86c2e7ca91dac3a3d7333ce9
dead_horse <dead_horse@qq.com>
2014-04-07 02:19:30 +0800

    refactor csrf example

--- csrf/app.js
+++ csrf/app.js
@@ -7,41 +7,29 @@ var route = require('koa-route');
 var app = module.exports = koa();
 
 /**
- * csrf middleware
- */
-
-csrf(app);
-
-/**
  * csrf need session
  */
+
 app.keys = ['session key', 'csrf example'];
 app.use(session());
 
 /**
- * assert CSRF token
+ * maybe a bodyparser
  */
 
-app.use(function* (next) {
-  if (this.method === 'GET'
-    || this.method === 'HEAD'
-    || this.method === 'OPTIONS') {
-    return yield* next;
-  }
-
-  // co-body or use other bodyparser middlewares
-  var body = yield parse(this);
-  try {
-    this.assertCSRF(body);
-  } catch (err) {
-    this.status = 403;
-    this.body = {
-      message: 'This CSRF token is invalid!'
-    };
-    return;
+app.use(function *(next) {
+  if (this.is('application/json')) {
+    this.request.body = yield parse(this);
   }
   yield* next;
 });
+
+/**
+ * csrf middleware
+ */
+
+app.use(csrf());
+
 
 /**
  * route
