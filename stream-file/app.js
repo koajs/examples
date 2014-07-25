fd5d7eba704d8a847c1b0d249930e2c82f9305c8
hemanth.hm <hemanth.hm@gmail.com>
2014-07-25 20:26:49 +0530

    Avoding internal server error.
    In case the file is not present in the requested path.

--- stream-file/app.js
+++ stream-file/app.js
@@ -6,10 +6,15 @@ var extname = path.extname;
 
 // try GET /app.js
 
-app.use(function *(){
+app.use(function * () {
   var path = __dirname + this.path;
-  this.type = extname(path);
-  this.body = fs.createReadStream(path);
+  if (fs.lstatSync(path).isFile()) {
+    this.type = extname(path);
+    this.body = fs.createReadStream(path);
+  } else {
+    this.satus = 404;
+    this.body = "File not found.";
+  }
 });
 
 if (!module.parent) app.listen(3000);
