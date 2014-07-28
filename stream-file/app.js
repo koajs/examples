934b9baddbe2fe5dc43c00aa3b156f7fcb5168e0
dead_horse <dead_horse@qq.com>
2014-07-28 23:39:15 +0800

    update stream-file

--- stream-file/app.js
+++ stream-file/app.js
@@ -6,23 +6,24 @@ var extname = path.extname;
 
 // try GET /app.js
 
-app.use(function * () {
+app.use(function *() {
   var path = __dirname + this.path;
   var fstat = yield stat(path);
 
   if (fstat.isFile()) {
     this.type = extname(path);
     this.body = fs.createReadStream(path);
-  } else {
-    this.satus = 404;
-    this.body = "File not found.";
-  }
-  
-  function stat(file) {
-    return function (done) {
-      fs.stat(file, done);
-    };
   }
 });
 
 if (!module.parent) app.listen(3000);
+
+/**
+ * thunkify stat
+ */
+
+function stat(file) {
+  return function (done) {
+    fs.stat(file, done);
+  };
+}
