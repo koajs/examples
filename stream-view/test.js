e41107e4ce758d8cbbf4207719bac6545f830daa
正寻 <chenqing.cq@alibaba-inc.com>
2015-09-09 16:03:49 +0800

    lost should

--- stream-view/test.js
+++ stream-view/test.js
@@ -1,5 +1,6 @@
 var app = require('./app');
 var request = require('supertest').agent(app.listen());
+var should  = require('should');
 
 describe('Stream View', function(){
   it('GET /', function(done){
