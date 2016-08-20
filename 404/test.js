1258133cee1cb0a9e7c0878a247b82a2ebd38170
Ilya Erokhin <i.erokhin@icloud.com>
2016-08-20 05:51:09 +0300

    test for 404 - no correct 404 page for exceptions in downstream middleware

--- 404/test.js
+++ 404/test.js
@@ -11,4 +11,20 @@ describe('404', function(){
       .expect(/Page Not Found/, done);
     })
   })
+  describe('when GET /exception', function(){
+    it('should return the 404 page', function(done){
+      request
+      .get('/exception')
+      .expect(404)
+      .expect(/Page Not Found/, done);
+    })
+  })
+  describe('when GET /ok', function(){
+    it('should 200 with "ok" message', function(done){
+      request
+      .get('/ok')
+      .expect(200)
+      .expect(/ok/, done);
+  	})
+  })
 })
